38a2b9d96375a25fcb70c7f74f780297dac13a9a
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-04-07 22:33:50 -0400

    Refactor MicroPython program for neopixel countdown
    Updated author information and added neopixel countdown functionality.

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,42 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Dat Nguyen
+Created on: Apr 2026
+This module will countdown neopixels.
 """
 
 from microbit import *
+from neopixel import NeoPixel
 
-display.scroll("Hello, World!")
+import time
+
+# define color constants
+BLACK = (0, 0, 0)
+REDD = (255, 0, 0)
+
+# initialize neopixel strip
+NEOPIXEL_STRIP = NeoPixel(pin16, 4)
+NEOPIXEL_STRIP.clear()
+NEOPIXEL_STRIP.show()
+
+# initialize display
+display.clear()
+display.show(Image.HAPPY)
+
+# main loop
+while True:
+    if button_a.was_pressed():
+        neopixel_counter = 0
+
+        # turn up all neopixels red
+        while neopixel_counter <= 3:
+            NEOPIXEL_STRIP[neopixel_counter] = RED
+
+        NEOPIXEL_STRIP.show()
+
+        # countdown neopixels
+        while neopixel_counter >= 0:
+            NEOPIXEL_STRIP[neopixel_counter] = BLACK
+            NEOPIXEL_STRIP.show()
+
+            # wait 1 s
+            time.sleep(1)
