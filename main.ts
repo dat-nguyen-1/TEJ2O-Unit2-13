4d7c6d567731e6603adeca4e82c9748a89e993b7
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-04-07 22:25:59 -0400

    neopixel countdown

--- main.ts
+++ main.ts
@@ -1,8 +1,35 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Dat Nguyen
+ * Created on: Apr 2026
+ * This program will countdown Neopixels.
 */
 
-basic.showString('Hello, World!')
+// define color constants
+const BLACK: NeoPixelColors = NeoPixelColors.Black
+const RED: NeoPixelColors = NeoPixelColors.Red
+
+// initialize neopixel instance
+const NEOPIXEL_STRIP = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+// initialize display
+basic.showIcon(IconNames.Yes)
+
+input.onButtonPressed(Button.A, function() {
+    let neopixelCounter: number = 0
+
+    // light up all neopixels
+    while (neopixelCounter <= 3) {
+        NEOPIXEL_STRIP.setPixelColor(neopixelCounter, RED)
+        neopixelCounter += 1
+    }
+
+    // neopixel countdown
+    while (neopixelCounter >= 0) {
+        NEOPIXEL_STRIP.setPixelColor(neopixelCounter, BLACK)
+        neopixelCounter -= 1
+
+        // wait 1 s
+        basic.pause(1000)
+    }
+})
